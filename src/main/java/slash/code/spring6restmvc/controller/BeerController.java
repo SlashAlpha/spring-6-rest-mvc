b540ad8bb75ceb5b61210e0001a81f012d78e770
Clément Duprat <jobclement@hotmail.fr>
2023-02-05 21:34:27 +0100

    added request in controller to get object by id

--- src/main/java/slash/code/spring6restmvc/controller/BeerController.java
+++ src/main/java/slash/code/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import slash.code.spring6restmvc.model.Beer;
 import slash.code.spring6restmvc.services.BeerService;
@@ -14,19 +16,20 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beers")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}",method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
         log.debug("GetBeer By Id in Beer Controller");
-       return beerService.getBeerById(id);
+       return beerService.getBeerById(beerId);
     }
 
 
