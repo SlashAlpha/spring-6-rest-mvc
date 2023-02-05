d6019f3796506537345e4a5bb6b449ddb7d1bc35
Clément Duprat <jobclement@hotmail.fr>
2023-02-05 22:09:54 +0100

    added devtools

--- src/main/java/slash/code/spring6restmvc/controller/CustomerController.java
+++ src/main/java/slash/code/spring6restmvc/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package slash.code.spring6restmvc.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import slash.code.spring6restmvc.services.CustomerService;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
@@ -28,6 +30,7 @@ public class CustomerController {
 
     @RequestMapping(value = "{customerId}",method = RequestMethod.GET)
     Customer getCustomerById(@PathVariable("customerId")  UUID customerId){
+        log.debug("GetCustomer By Id in Customer Controller");
        return customerService.getCustomerById(customerId);
     }
 
