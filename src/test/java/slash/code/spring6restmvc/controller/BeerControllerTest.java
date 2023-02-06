2aa19b788c619dec7db36ff4cdc20b05f27c6f9a
Clément Duprat <jobclement@hotmail.fr>
2023-02-06 10:16:10 +0100

    list of object test

--- src/test/java/slash/code/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/slash/code/spring6restmvc/controller/BeerControllerTest.java
@@ -10,13 +10,11 @@ import slash.code.spring6restmvc.model.Beer;
 import slash.code.spring6restmvc.services.BeerService;
 import slash.code.spring6restmvc.services.BeerServiceImpl;
 
-import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@SpringBootTest
 @WebMvcTest(BeerController.class)
@@ -37,12 +35,28 @@ class BeerControllerTest {
 
         Beer testBeer=beerServiceImpl.listBeers().get(0);
 
-        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
 
-        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID())
+        mockMvc.perform(get("/api/v1/beer/"+ testBeer.getId())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id",is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName",is(testBeer.getBeerName())));
     }
+
+    @Test
+    void testListBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",is(3)));
+
+
+    }
+
 }
