6a8e17a32d486957de7c4e831a8a42da4db6b704
Clément Duprat <jobclement@hotmail.fr>
2023-02-10 10:22:09 +0100

    not found exception test management

--- src/test/java/slash/code/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/slash/code/spring6restmvc/controller/BeerControllerIT.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import slash.code.spring6restmvc.entities.Beer;
 import slash.code.spring6restmvc.model.BeerDTO;
 import slash.code.spring6restmvc.repositories.BeerRepository;
 
+import java.rmi.NotBoundException;
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
@@ -21,11 +26,28 @@ class BeerControllerIT {
     @Autowired
     BeerRepository beerRepository;
 
+    @Test
+    void testGetBeerIdNotFound() {
+        assertThrows(NotFoundException.class,()->{
+            beerController.getBeerById(UUID.randomUUID());
+        });
+
+
+    }
 
     @Test
     void testListBeers() {
         List<BeerDTO>dtos=beerController.listBeers();
         assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Test
+    void getBeerByIdTest() {
+        Beer beer=beerRepository.findAll().get(0);
+        BeerDTO dto=beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+
     }
 
     @Rollback
